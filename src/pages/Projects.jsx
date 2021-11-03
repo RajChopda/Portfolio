0731c742f32a9e52587bcc20e6772bf12c2d23a2
RajChopda <70621945+RajChopda@users.noreply.github.com>
2021-11-03 11:22:52 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,7 +9,7 @@ export default function Projects() {
         <>
             <NavBar projects='active' />
             <div className="w-50 mx-auto">
-                <ProjectCardStruct name='Secure Files' type='Java Desktop Application' discription='Secure your any format file and text by your key.' link='#SecureFiles' />
+                <ProjectCardStruct name='Secure Files' type='Java Desktop Application' discription='Secure your any format file and text by your own key.' link='#SecureFiles' />
                 <ProjectCardStruct name='E-Classroom' type='Web Application' discription='Announcement by post, share files to your students and take attendance.' link='#E-Classroom' />
                 <ProjectCardStruct name='Online Voting App' type='Java Desktop Application' discription='Give vote from home. #StayHomeStaySafe' link='#OnlineVotingApp' />
                 <PageBottomMargin />
